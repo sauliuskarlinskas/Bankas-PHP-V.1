85ba02bcee485c0c333b811bf092b3813dd3415c
sauliuskarlinskas <127085630+sauliuskarlinskas@users.noreply.github.com>
2023-06-05 10:03:16 +0300

    v1 bank start

---
+++ main.php
@@ -0,0 +1,54 @@
+<?php
+
+$page = (int) ($_GET['p'] ?? 1);
+
+if (!in_array($page, [1, 2, 3])) {
+    header('Location: http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php');
+    die();
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>newAccount</title>
+
+</head>
+
+<body>
+
+<nav>
+    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php?p=1" >Pagrindinis puslapis</a>
+    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/newAccount.php?p=2" >Naujos sąskaitos sukūrimas</a>
+    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/moneyIn.php?p=3" >Pridėti lėšų</a>
+    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/moneyOut.php?p=4" >Nuskaičiuoti lėšas</a>
+</nav>
+
+<?php if($page == 1) : ?>
+
+<h1>Pagrindinis puslapis</h1>
+
+<?php elseif($page == 2) : ?>
+
+<h1>Page 2</h1>
+
+<?php elseif($page ==3) : ?>
+
+<h1>Page 3</h1>
+
+<?php else : ?>
+
+<h1>Page 4</h1>
+
+<?php endif ?>
+
+</body>
+
+</html>
---
+++ newAccount.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>newAccount</title>
+
+</head>
+
+<body>
+    <h1>Naujos sąskaitos sukūrimas</h1>
+
+    <fieldset>
+        <form>
+            <div>
+                <label>Vardas</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Pavardė</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Asmens kodas</label>
+                <input type="text">
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+
+        </form>
+    </fieldset>
+
+    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php?p=1" >Grįžti į pagrindinį puslapį</a>
+</body>
+
+</html>
