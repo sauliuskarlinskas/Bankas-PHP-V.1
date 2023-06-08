37e69490ced193c281bb14eaa3fc964b3ed4ad83
sauliuskarlinskas <127085630+sauliuskarlinskas@users.noreply.github.com>
2023-06-08 10:50:45 +0300

    bootstrap

--- editBalance.php
+++ editBalance.php
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         return $u;
     }, $usersData);
+
     $usersData = json_encode($usersData);
     file_put_contents(__DIR__ . '/usersData.json', $usersData);
     header('Location: http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php');
@@ -31,25 +32,28 @@ $userData = array_shift($userData);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>editBalance</title>
 
 </head>
 
 <body>
-    <div>
-        <div>
+    <div class="container">
+        <div class="col m-5">
             <h2>Lėšų pridėjimas</h2>
         </div>
-        <div>
-            <form>
+        <div class="col m-5">
+            <form action="./editBalance.php?id=<?= $userData['id'] ?>" method="post">
                 <label>Pridėti lėšų</label>
-                <input type="number" name="likutis" value="<?=$userData['likutis'] ?>" >
-                <button>Išsaugoti</button>
+                <input type="number" name="likutis" value="<?= $userData['likutis'] ?>">
+                <button class="btn btn-success">Išsaugoti</button>
             </form>
         </div>
-
-
-        <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php">Grįžti į pagrindinį puslapį</a>
+        <div class="col m-5">
+            <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/main.php" class="btn btn-info">Grįžti į pagrindinį
+                puslapį</a>
+        </div>
     </div>
 </body>
 
--- main.php
+++ main.php
@@ -14,51 +14,67 @@ $usersData = $usersData ? json_decode($usersData, 1) : [];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>newAccount</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Pagrindinis puslapis</h1>
-    <div>
-        <nav>
-            <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/newAccount.php?p=1">Naujos sąskaitos sukūrimas</a>
+    <div class="container">
+        <div class="col m-5">
+            <h1>Pagrindinis puslapis</h1>
+        </div>
+        <div class="col m-5">
+            <nav>
+                <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/newAccount.php?p=1" class="btn btn-info">Naujos
+                    sąskaitos sukūrimas</a>
 
-        </nav>
+            </nav>
+        </div>
+
+        <div class="col">
+            <table class="table table-dark table-striped">
+
+                <thead>
+                    <tr>
+                        <th scope="col">Vardas</th>
+                        <th scope="col">Pavarde</th>
+                        <th scope="col">Asmens kodas</th>
+                        <th scope="col">Saskaitos numeris</th>
+                        <th scope="col">Likutis</th>
+                    </tr>
+                </thead>
+                <?php foreach ($usersData as $userData): ?>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <?= $userData['vardas']; ?>
+                            </td>
+                            <td>
+                                <?= $userData['pavarde']; ?>
+                            </td>
+                            <td>
+                                <?= $userData['asmensKodas']; ?>
+                            </td>
+                            <td>
+                                <?= $userData['saskaitosNumeris']; ?>
+                            </td>
+                            <td>
+                                <?= $userData['likutis']; ?>
+                            </td>
+                            <td>
+                                <form action="./deleteAccount.php?id=<?= $userData['id'] ?>" method="post">
+                                    <button type="submit" class="btn btn-danger">delete</button>
+                                    <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/editBalance.php"
+                                        class="btn btn-secondary">edit</a>
+                                </form>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                <?php endforeach ?>
+            </table>
+        </div>
     </div>
-    <div>
-        <table>
-            <?php foreach ($usersData as $userData): ?>
-                <tr>
-
-                    <td>
-                        <?= $userData['vardas']; ?>
-                    </td>
-                    <td>
-                        <?= $userData['pavarde']; ?>
-                    </td>
-                    <td>
-                        <?= $userData['asmensKodas']; ?>
-                    </td>
-                    <td>
-                        <?= $userData['saskaitosNumeris']; ?>
-                    </td>
-                    <td>
-                        <?= $userData['likutis']; ?>
-                    </td>
-                    <td>
-                        <form action="./deleteAccount.php?id=<?= $userData['id'] ?>" method="post">
-                            <button type="submit" >delete</button>
-                            <a href="http://localhost/zuikiai/Bankas%20PHP%20V.1/editBalance.php">edit</a>
-                        </form>
-                    </td>
-                </tr>
-            <?php endforeach ?>
-        </table>
-
-    </div>
-
-
-
 
 </body>
 
